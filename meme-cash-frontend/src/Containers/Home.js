adbead53dbb15ee34baee6c41e0fddbb13323cfe
Harsh Agrawal <harshagrawal.dhn@gmail.com>
2020-10-27 12:23:04 +0530

    Add files via upload

---
+++ meme-cash-frontend/src/Containers/Home.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Card from "./Card";
+import "../CSS/Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <Navbar />
+      <div className="memes">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
